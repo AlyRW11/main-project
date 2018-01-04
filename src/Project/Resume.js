5327882fd8e96c5b59228f84832b23c2acf00807
alyr.wayne <alegab89@hotmail.co.uk>
2018-01-04 13:09:41 -0700

    styling Resume css

--- src/Project/Resume.js
+++ src/Project/Resume.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Resume.css'
 
 class Resume extends Component {
     render () {
@@ -34,41 +35,43 @@ class Resume extends Component {
              <div>
                 <h2>Experience</h2>
                 <h3>Verizon Wireless 2010 to Present </h3>
-                <div>
-                    <h5>Global Enterprise Advisor 2014 to Present</h5>
-                    <ul>
-                        <li>Account Concerns & Escalations</li>
-                        <li>Account Recommendations</li>
-                        <li>Invoice Related Questions</li>
-                        <li>Reporting & Research</li>
-                    </ul>
-                </div>
-                <div>
-                    <h5>Business and Goverment Phone Support Cordinator 2012 to 2014</h5>
-                    <ul>
-                    <li>Communicate with Impact - Ability to provide information that will help a with a business decisions.</li>
-                        <li>Drive Metrics - Ability to move PA number to leading numbers.</li>
-                        <li>Systems Agility - Highly proficient with billing system ACSS, My Business and Thin Space.</li>
-                        <li>Track and reported my team’s Company NPS. </li>
-                    </ul>
-                </div>
-                <div>
-                    <h5>Consumer Customer Service 2011 to 2012</h5>
-                    <ul>
-                        <li>Ensuring day to day successful operations by providing effective two-way feedback to motivate a high performance and a productive environment.</li>
-                        <li>Provide day to day coaching to Representatives; to help increase key playbook initiatives (customer loyalty, customer experience, and efficiencies) ensuring first call resolution, and driving top box ACS scores.</li>
-                        <li>Responsible for providing leadership and development to bilingual teams.</li><li>Systems Agility – Highly proficient Microsoft Office Suite </li>
-                    </ul>
-                </div>
-                <div> 
-                    <h5>Consumer Bilingual Customer Service 2010 to 2011</h5>
-                    <ul>
-                        <li>Educate, answer and resolve customers concerns.</li>
-                        <li>Ensure customer satisfaction by providing excellent communications and customer service skills.</li>
-                        <li>Improve team quality by sharing best practices and ideas to my peers</li>
-                        <li>Took bilingual calls to assist customer with billing information.</li>
-                    </ul>
-                </div>
+                <div class="roles">   
+                    <div>
+                        <h5>Global Enterprise Advisor 2014 to Present</h5>
+                        <ul>
+                            <li>Account Concerns & Escalations</li>
+                            <li>Account Recommendations</li>
+                            <li>Invoice Related Questions</li>
+                            <li>Reporting & Research</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h5>Business and Goverment Phone Support Cordinator 2012 to 2014</h5>
+                        <ul>
+                        <li>Communicate with Impact - Ability to provide information that will help a with a business decisions.</li>
+                            <li>Drive Metrics - Ability to move PA number to leading numbers.</li>
+                            <li>Systems Agility - Highly proficient with billing system ACSS, My Business and Thin Space.</li>
+                            <li>Track and reported my team’s Company NPS. </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h5>Consumer Customer Service 2011 to 2012</h5>
+                        <ul>
+                            <li>Ensuring day to day successful operations by providing effective two-way feedback to motivate a high performance and a productive environment.</li>
+                            <li>Provide day to day coaching to Representatives; to help increase key playbook initiatives (customer loyalty, customer experience, and efficiencies) ensuring first call resolution, and driving top box ACS scores.</li>
+                            <li>Responsible for providing leadership and development to bilingual teams.</li><li>Systems Agility – Highly proficient Microsoft Office Suite </li>
+                        </ul>
+                    </div>
+                    <div> 
+                        <h5>Consumer Bilingual Customer Service 2010 to 2011</h5>
+                        <ul>
+                            <li>Educate, answer and resolve customers concerns.</li>
+                            <li>Ensure customer satisfaction by providing excellent communications and customer service skills.</li>
+                            <li>Improve team quality by sharing best practices and ideas to my peers</li>
+                            <li>Took bilingual calls to assist customer with billing information.</li>
+                        </ul>
+                    </div>
+                 </div>   
              </div>
          </div>
         )
