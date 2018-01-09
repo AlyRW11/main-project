c5a762adcfc88f025027ee526cdc2c4669b1e61a
alyr.wayne <alegab89@hotmail.co.uk>
2018-01-08 19:36:13 -0700

    testing the interactivity

--- src/Project/Testing.js
+++ src/Project/Testing.js
@@ -3,8 +3,22 @@ import Particles from 'react-particles-js'
 import './Testing.css'
 
 class Test extends Component {
+    state = {
+        
+    }
 
     render() {
+        if(params){
+            Object.deepExtend(pJS, params);
+          }
+        
+          pJS.tmp.obj = {
+            size_value: pJS.particles.size.value,
+            size_anim_speed: pJS.particles.size.anim.speed,
+            move_speed: pJS.particles.move.speed,
+            mode_repulse_distance: pJS.interactivity.modes.repulse.distance
+          }
+
         return(
             <Particles className="particle-animation"
                 params={{
@@ -113,6 +127,9 @@ class Test extends Component {
                         retina_detect: true,
                     }
                 }}/>
+
+
+                
         )
     }
 }
