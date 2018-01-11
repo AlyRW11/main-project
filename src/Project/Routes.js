b66c18167d43364bff16b8fb32b2849ac70d4333
alyr.wayne <alegab89@hotmail.co.uk>
2018-01-11 12:38:59 -0700

    testing state in the testTWo file

--- src/Project/Routes.js
+++ src/Project/Routes.js
@@ -4,7 +4,7 @@ import AbtMe from './Personal.js'
 import Resume from './Resume.js'
 import Pro from './Projects.js'
 import Test from './Testing.js'
-// import TestTwo from './TestTwo.js'
+import TestTwo from './TestTwo.js'
 
 
 class Routes extends Component {
@@ -17,14 +17,14 @@ class Routes extends Component {
                 <Link to="/resume">Resume </Link>
                 <Link to="/projects">Projects </Link>
                 <Link to="/Test">Test</Link>
-                {/* <Link to="/TestTwo">Test 2 </Link> */}
+                <Link to="/TestTwo">Test 2 </Link>
             </nav>
             <Route exact path="/" component={AbtMe}/>
             <Route path="/resume" component={() => <Resume/>}/>
             {/* if you do {() => <“something”/>}/> then it will run props */}
             <Route path="/projects" component={() => <Pro/>}/>
             <Route path="/test" component={() => <Test/>}/>
-            {/* <Route path="/testTwo" component={() => <TestTwo/>}/> */}
+            <Route path="/testTwo" component={() => <TestTwo/>}/>
                 </div>
             </Router>
             
